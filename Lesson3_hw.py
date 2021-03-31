2deb558a2ceb2f780466bedde90f0865e0d66a5f
StevenChou0323 <kane3446@gmail.com>
2021-04-01 01:00:16 +0800

    習題

--- Lesson3_hw.py
+++ Lesson3_hw.py
@@ -2,3 +2,22 @@ name = "sdtete1223!!!023"
 #name = "sdtete1223023"
 #檢查是不是字串單純只有字母跟數字的神器
 print(name.isalnum())
+
+song = """When an eel grabs your arm,
+    And it causes great harm,
+    That's a - a moray!"""
+
+arg = ('roast beef', 'fish') 
+
+print('''My kitty cat likes %s,
+My kitty cat likes %s''' % arg)
+
+
+print(song.replace(' m', ' M'))
+
+
+
+print('''Dear {salutation} {name},
+we are sorry that our {product}
+is in your room'''.format(salutation = 'Ambassador', name = 'Steven', product = 'shampoo')
+)
